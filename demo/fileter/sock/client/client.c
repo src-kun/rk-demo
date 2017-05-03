bde35a0c8cbcf232c80eaa1a6d0483c7a833249a
root <root@localhost.localdomain>
2017-05-03 11:55:09 +0800

    修复socket交互数据内核崩溃bug

--- demo/fileter/sock/client/client.c
+++ demo/fileter/sock/client/client.c
@@ -1,80 +1,86 @@
-
-    /*client.c*/  
-    #include<linux/in.h>  
-    #include<linux/inet.h>  
-    #include<linux/socket.h>  
-    #include<net/sock.h>  
+#include<linux/in.h>  
+#include<linux/inet.h>  
+#include<linux/socket.h>  
+#include<net/sock.h>  
+  
+#include<linux/init.h>  
+#include<linux/module.h>  
+  
+int myclient(void){  
+    struct socket *sock;  
+    struct sockaddr_in s_addr;  
+    unsigned short portnum = 0x8870;  
+    int ret = 0;  
+  
+    memset(&s_addr,0,sizeof(s_addr));  
+    s_addr.sin_family = AF_INET;  
+    s_addr.sin_port = htons(portnum);  
+    //s_addr.sin_addr.s_addr=inet_addr("127.0.0.1"); /*I am not sure whether the ined_addr is right, may should be htonl*/  
+    s_addr.sin_addr.s_addr=in_aton("127.0.0.1");  
+    sock = (struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
       
-    #include<linux/init.h>  
-    #include<linux/module.h>  
-      
-    int myclient(void){  
-            struct socket *sock;  
-            struct sockaddr_in s_addr;  
-            unsigned short portnum=8080;  
-            int ret=0;  
-      
-            memset(&s_addr,0,sizeof(s_addr));  
-            s_addr.sin_family=AF_INET;  
-            s_addr.sin_port=htons(portnum);  
-           
-            s_addr.sin_addr.s_addr=in_aton("192.168.189.159"); /*server ip is 192.168.209.134*/  
-            sock=(struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
-      
-            /*create a socket*/  
-            ret=sock_create_kern(AF_INET, SOCK_STREAM,0,&sock);  
-            if(ret<0){  
-                    printk("client:socket create error!\n");  
-                    return ret;  
-            }  
-            printk("client: socket create ok!\n");  
-      
-            /*connect server*/  
-            ret=sock->ops->connect(sock,(struct sockaddr *)&s_addr, sizeof(s_addr),0);  
-            if(ret!=0){  
-                    printk("client:connect error!\n");  
-                    return ret;  
-            }  
-            printk("client:connect ok!\n");  
-      
-            /*kmalloc sendbuf*/  
-            char *sendbuf=NULL;  
-            sendbuf=kmalloc(1024,GFP_KERNEL);  
-            if(sendbuf==NULL){  
-                    printk("client: sendbuf kmalloc error!\n");  
-                    return -1;  
-            }  
-            memset(sendbuf,1,1024);          
-              
-        struct kvec vec;  
-            struct msghdr msg;  
-      
-            vec.iov_base=sendbuf;  
-            vec.iov_len=1024;  
-      
-            memset(&msg,0,sizeof(msg));  
-      
-            ret=kernel_sendmsg(sock,&msg,&vec,1,1024); /*send message */  
-            if(ret<0){  
-                    printk("client: kernel_sendmsg error!\n");  
-                    return ret;  
-            }else if(ret!=1024){  
-                    printk("client: ret!=1024");  
-            }  
-            printk("client:send ok!\n");  
-      
-            return ret;  
+    /*create a socket*/  
+    ret = sock_create_kern(AF_INET, SOCK_STREAM,0,&sock);  
+    if(ret < 0){  
+        printk("client:socket create error!\n");  
+        return ret;  
     }  
-      
-    static int client_init(void){  
-            printk("client:init\n");  
-            return (myclient());  
+    printk("client: socket create ok!\n");  
+  
+    /*conncet server*/  
+    ret = sock->ops->connect(sock,(struct sockaddr *)&s_addr, sizeof(s_addr),0);  
+    if(ret != 0){  
+        printk("client:connect error!\n");  
+        return ret;  
     }  
+    printk("client:connect ok!\n");  
       
-    static void client_exit(void){  
-            printk("client exit!\n");  
+    /*kmalloc sendbuf*/ 
+    char *args = "this is test message!";
+    char *sendbuf = NULL;  
+    int len = 0;  
+    sendbuf = kmalloc(1024,GFP_KERNEL);
+	if(sendbuf==NULL){  
+		printk("client: sendbuf kmalloc error!\n");  
+		return -1;  
+	}  
+	memset(sendbuf, 0, 1024);
+	
+	len = strlen(args) + 1;
+	memcpy(sendbuf, args, len);
+	printk("args len %d \n", strlen(args));
+    /*send message */  
+    struct kvec vec;  
+    struct msghdr msg;  
+      
+    vec.iov_base = sendbuf;  
+    vec.iov_len = len;  
+    memset(&msg,0,sizeof(msg));  
+      
+    ret = kernel_sendmsg(sock,&msg,&vec,1,len);  
+    if(ret < 0){  
+        printk("client: kernel_sendmsg error!\n");  
+        return ret;   
+    }else if(ret != len){  
+        printk("client: ret!=len\n");  
     }  
-      
-    module_init(client_init);  
-    module_exit(client_exit);  
-    MODULE_LICENSE("GPL"); 
+    printk("client send size=%d\n",ret);  
+    printk("client:send ok!\n");  
+  
+    sock_release(sock);  
+    return ret;  
+}  
+  
+static int client_init(void){  
+    printk("client:init\n");  
+    myclient();  
+    return 0;  
+}  
+  
+static void client_exit(void){  
+    printk("client exit!\n");  
+}  
+  
+module_init(client_init);  
+module_exit(client_exit);  
+MODULE_LICENSE("GPL"); 
--- demo/fileter/sock/server/server.c
+++ demo/fileter/sock/server/server.c
@@ -1,4 +1,3 @@
- /*server.c*/  
 #include<linux/in.h>  
 #include<linux/inet.h>  
 #include<linux/socket.h>  
@@ -8,85 +7,96 @@
 #include<linux/module.h>  
   
 int myserver(void){  
+	  
+	struct socket *sock,*client_sock;  
+	struct sockaddr_in s_addr;  
+	unsigned short portnum=0x8870;  
+	int ret=0;  
   
-        struct socket *sock,*client_sock;  
-        struct sockaddr_in s_addr;  
-        unsigned short portnum=8080;  
-        int ret=0;  
-  
-        memset(&s_addr,0,sizeof(s_addr));  
-        s_addr.sin_family=AF_INET;  
-        s_addr.sin_port=htons(portnum);  
-        s_addr.sin_addr.s_addr=htonl(INADDR_ANY);  
+	memset(&s_addr,0,sizeof(s_addr));  
+	s_addr.sin_family=AF_INET;  
+	s_addr.sin_port=htons(portnum);  
+	s_addr.sin_addr.s_addr=htonl(INADDR_ANY);  
   
   
-        sock=(struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
-        client_sock=(struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
+	sock=(struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
+	client_sock=(struct socket *)kmalloc(sizeof(struct socket),GFP_KERNEL);  
   
-        /*create a socket*/  
-        ret=sock_create_kern(AF_INET, SOCK_STREAM,0,&sock);  
-        if(ret){  
-                printk("server:socket_create error!\n");  
-        }  
-        printk("server:socket_create ok!\n");  
+	/*create a socket*/  
+	ret=sock_create_kern(AF_INET, SOCK_STREAM,0,&sock);  
+	if(ret){  
+		printk("server:socket_create error!\n");  
+	}  
+	printk("server:socket_create ok!\n");  
   
-        /*bind the socket*/  
-        ret=sock->ops->bind(sock,(struct sockaddr *)&s_addr,sizeof(struct sockaddr_in));  
-        if(ret<0){  
-                printk("server: bind error\n");  
-                return ret;  
-        }  
-        printk("server:bind ok!\n");  
+	/*set the socket can be reused*/  
+	int val=1;  
+	ret= kernel_setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(char *)&val,sizeof(val));  
+	if(ret){  
+		printk("kernel_setsockopt error!!!!!!!!!!!\n");  
+	}  
   
-        /*listen*/  
-        ret=sock->ops->listen(sock,10);  
-        if(ret<0){  
-                printk("server: listen error\n");  
-                return ret;  
-        }  
-        printk("server:listen ok!\n");  
+	/*bind the socket*/  
+	ret=sock->ops->bind(sock,(struct sockaddr *)&s_addr,sizeof(struct sockaddr_in));  
+	if(ret<0){  
+		printk("server: bind error\n");  
+		return ret;  
+	}  
+	printk("server:bind ok!\n");  
   
-        ret=sock->ops->accept(sock,client_sock,10);if(ret<0){  
-                printk("server:accept error!\n");  
-                return ret;  
-        }  
+	/*listen*/  
+	ret=sock->ops->listen(sock,10);  
+	if(ret<0){  
+		printk("server: listen error\n");  
+		return ret;  
+	}  
+	printk("server:listen ok!\n");  
   
-        printk("server: accept ok, Connection Established\n");  
+	//ret=sock->ops->accept(sock,client_sock,10);  
+	ret = kernel_accept(sock,&client_sock,10);  
+	if(ret<0){  
+		printk("server:accept error!\n");  
+		return ret;  
+	}  
+	  
+	printk("server: accept ok, Connection Established\n");  
+	  
+	/*kmalloc a receive buffer*/  
+	char *recvbuf=NULL;  
+	recvbuf=kmalloc(1024,GFP_KERNEL);  
+	if(recvbuf==NULL){  
+		printk("server: recvbuf kmalloc error!\n");  
+		return -1;  
+	}  
+	memset(recvbuf, 0, sizeof(recvbuf));  
+	  
+	/*receive message from client*/  
+	struct kvec vec;  
+	struct msghdr msg;  
+	memset(&vec,0,sizeof(vec));  
+	memset(&msg,0,sizeof(msg));  
+	vec.iov_base=recvbuf;  
+	vec.iov_len=1024;  
+	ret=kernel_recvmsg(client_sock, &msg, &vec, 1, 1024, 0);  
+	printk("receive message: %s\n", recvbuf);  
+	printk("receive size=%d\n", ret);  
   
-        /*kmalloc a receive buffer*/  
-        char *recvbuf=NULL;  
-        recvbuf=kmalloc(1024,GFP_KERNEL);  
-        if(recvbuf==NULL){  
-                printk("server: recvbuf kmalloc error!\n");  
-                return -1;  
-        }  
-        memset(recvbuf, 0, sizeof(recvbuf));  
-  
-        /*receive message from client*/  
-        struct kvec vec;  
-        struct msghdr msg;  
-        memset(&vec,0,sizeof(vec));  
-        memset(&msg,0,sizeof(msg));  
-        vec.iov_base=recvbuf;  
-        vec.iov_len=1024;  
-        ret=kernel_recvmsg(client_sock,&msg,&vec,1,1024,0); /*receive message*/  
-        printk("receive message:\n %s\n",recvbuf);  
-          
-    /*release socket*/  
-    sock_release(sock);  
-        sock_release(client_sock);  
-        return ret;  
+	sock_release(sock);  
+	sock_release(client_sock);  
+	return ret;  
 }  
   
 static int server_init(void){  
-        printk("server init:\n");  
-        return (myserver());  
-}  
+	printk("server init:\n");  
+	myserver();  
+	return 0;  
+}         
   
 static void server_exit(void){  
-        printk("good bye\n");  
+	printk("good bye\n");  
 }  
   
 module_init(server_init);  
 module_exit(server_exit);  
   
+MODULE_LICENSE("GPL");  
